4c2f151260e576c09a2f863f1d4ae8570d20845f
madebytimo <t.schnaible@madebytimo.de>
2026-07-11 15:50:37 +0200

    also set the selected model to `AI_MODEL`

--- files/run-hermes.sh
+++ files/run-hermes.sh
@@ -14,7 +14,8 @@ export SIGNAL_HOME_CHANNEL SIGNAL_HTTP_URL
 su user --whitelist-environment \
     BRAVE_API_KEY,SIGNAL_ACCOUNT,SIGNAL_ALLOWED_USERS,SIGNAL_GROUP_ALLOWED_USERS,SIGNAL_HOME_CHANNEL,SIGNAL_HTTP_URL \
     --command \
-    "hermes config set model.provider container-config \
+    "hermes config set model.default '$AI_MODEL' \
+    && hermes config set model.provider container-config \
     && hermes config set providers.container-config.base_url '$AI_API_URL' \
     && hermes config set providers.container-config.api_key '$AI_API_KEY' \
     && hermes config set providers.container-config.default_model '$AI_MODEL' \
